72e687cafd8470b4a27e3f6852b8b74b0aad3a40
Aditya Pradhan <adiyouaresosmart@gmail.com>
2024-08-21 14:27:18 +0530

    Day 2: Secondary Commit

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,9 @@
+let s1 = " Hello";
+let s2 = " Adi";
+
+console.log(s1 + s2);
+
+console.log(1 + "2" + 1); //121
+console.log(1 + 2 + "1"); //31
+console.log("1" + 2 + 1); //121
+console.log(+""); //0
